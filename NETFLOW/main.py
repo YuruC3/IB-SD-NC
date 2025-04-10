bfd5228dc18f91ed80f85ad6000a9ff0778bad45
YuruC3 <98943911+YuruC3@@users.noreply.github.com>
2025-04-10 12:15:12 +0200

    .

--- NETFLOW/main.py
+++ NETFLOW/main.py
@@ -8,3 +8,19 @@ payload, client = sock.recvfrom(4096)  # experimental, tested with 1464 bytes
 p = netflow.parse_packet(payload)  # Test result: <ExportPacket v5 with 30 records>
 
 print(p.header.version)  # Test result: 5
+
+
+# Duplicate IP flow
+pkt = IP(len=16384, src='192.168.240.243', dst=ip,
+        id=RandShort(), ttl=64)/TCP(sport=5000,
+        dport=5000, flags="S", window=200,
+        options=[('MSS', 1460), ('WScale', 2)])/CustomLayer(type=1, update=2)/"SENT"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:    
+    s.connect((ip, 5000))
+except socket.error:
+    print("User not connected")
+
+spkt = str(pkt)
+s.send(spkt)
