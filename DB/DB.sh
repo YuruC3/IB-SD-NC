861bdb6988f1b87525ab1aed71f17bec89debfa7
Bubbis03 <alexander@fyrpihl.se>
2025-04-13 20:44:14 +0200

    La till resten av databaserna samt skapade ett event som dagligen raderar netflow data som är äldre än 30 dagar.

--- DB/DB.sh
+++ DB/DB.sh
@@ -16,3 +16,11 @@ use Netflow_Data;
 create table data (ID int, IPV4_SRC_ADDR varchar(100), IPV4_DST_ADDR varchar(100), NEXT_HOP varchar(100), INPUT varchar(100), OUTPUT varchar (100),
  IN_PACKETS varchar(100), IN_OCTETS varchar(100), FIRST_SWITCHED varchar(100), LAST_SWITCHED varchar(100), SRC_PORT varchar(100), DST_PORT varchar(100),
  TCP_FLAGS varchar(100), PROTO varchar(100), TOS varchar(100), SRC_AS varchar(100), DST_AS varchar(100), SRC_MASK varchar(100), DST_MASK varchar(100), DATE date);
+
+create event prune_data on schedule every 1 day do delete from data where 2592000 <= UNIX_TIMESTAMP(DATE);
+create database Switch_Conf;
+use Switch_Conf;
+create table conf (ID int, CONF TEXT(65535), DATE date);
+create database QoS_Log;
+use QoS_Log;
+create table logs (ID int, PROBLEM varchar(255), SOLUTION varchar(255), DATE date);
