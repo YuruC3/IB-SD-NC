85886aec7cbb08d7c9fcf92c03db53b47915d131
YuruC3 <98943911+YuruC3@@users.noreply.github.com>
2025-04-14 14:01:27 +0200

    Kanske fungerande netflow???

--- NETFLOW/netflow.py
+++ NETFLOW/netflow.py
@@ -6,7 +6,7 @@ import json
 # DB
 from sqlmodel import SQLModel, Field, create_engine, Session
 from typing import Optional
-from classes import *
+from classes import data
 
 # DB conf
 DB_HOST = "192.168.1.63"
@@ -19,6 +19,14 @@ DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DATABSE
 
 # Init db engine
 engine = create_engine(DATABASE_URL)
+
+def InsertDataToMariaDB(inpData: data):
+    with Session(engine) as session:
+        session.add(inpData)
+        if session.commit():
+            return 0
+        else:
+            return 1
 
 def netflowColleciton():
     i=1
@@ -37,6 +45,14 @@ def netflowColleciton():
         ip_address_fixed = socket.inet_ntoa(struct.pack('!I', ip)) 
         tempEntry['IPV4_SRC_ADDR'] = ip_address_fixed
         
+        ip = int(tempEntry['IPV4_DST_ADDR'])
+        ip_address_fixed = socket.inet_ntoa(struct.pack('!I', ip)) 
+        tempEntry['IPV4_DST_ADDR'] = ip_address_fixed
+        
+        ip = int(tempEntry['NEXT_HOP'])
+        ip_address_fixed = socket.inet_ntoa(struct.pack('!I', ip)) 
+        tempEntry['NEXT_HOP'] = ip_address_fixed
+        
         first = int(tempEntry['FIRST_SWITCHED'])
         last = int(tempEntry['LAST_SWITCHED'])
         firstreadable = str(timedelta(milliseconds=first))
@@ -50,26 +66,30 @@ def netflowColleciton():
 
         # Database thing
         tmpDbEntry = data(
-            IPV4_SRC_ADDR=inEntry["IPV4_SRC_ADDR"],
-            IPV4_DST_ADDR=inEntry["IPV4_DST_ADDR"],
-            NEXT_HOP=inEntry["NEXT_HOP"],
-            INPUT=inEntry["INPUT"],
-            OUTPUT=inEntry["OUTPUT"],
-            IN_PACKETS=inEntry["IN_PACKETS"],
-            IN_OCTETS=inEntry["IN_OCTETS"],
-            FIRST_SWITCHED=inEntry["FIRST_SWITCHED"],
-            LAST_SWITCHED=inEntry["LAST_SWITCHED"],
-            SRC_PORT=inEntry["SRC_PORT"],
-            DST_PORT=inEntry["DST_PORT"],
-            TCP_FLAGS=inEntry["TCP_FLAGS"],
-            PROTO=inEntry["PROTO"],
-            TOS=inEntry["TOS"],
-            SRC_AS=inEntry["SRC_AS"],
-            DST_AS=inEntry["DST_AS"],
-            SRC_MASK=inEntry["SRC_MASK"],
-            DST_MASK=inEntry["DST_MASK"],
+            IPV4_SRC_ADDR=tempEntry["IPV4_SRC_ADDR"],
+            IPV4_DST_ADDR=tempEntry["IPV4_DST_ADDR"],
+            NEXT_HOP=tempEntry["NEXT_HOP"],
+            INPUT=tempEntry["INPUT"],
+            OUTPUT=tempEntry["OUTPUT"],
+            IN_PACKETS=tempEntry["IN_PACKETS"],
+            IN_OCTETS=tempEntry["IN_OCTETS"],
+            FIRST_SWITCHED=tempEntry["FIRST_SWITCHED"],
+            LAST_SWITCHED=tempEntry["LAST_SWITCHED"],
+            SRC_PORT=tempEntry["SRC_PORT"],
+            DST_PORT=tempEntry["DST_PORT"],
+            TCP_FLAGS=tempEntry["TCP_FLAGS"],
+            PROTO=tempEntry["PROTO"],
+            TOS=tempEntry["TOS"],
+            SRC_AS=tempEntry["SRC_AS"],
+            DST_AS=tempEntry["DST_AS"],
+            SRC_MASK=tempEntry["SRC_MASK"],
+            DST_MASK=tempEntry["DST_MASK"],
             DATE=date.today()
             )
+        
+        # Send to DB
+        InsertDataToMariaDB(tmpDbEntry)
+        tmpDbEntry = data()
 
 
         
