bdf83b61e615dc17be3e2c8b7ed5462464fc6c08
YuruC3 <98943911+YuruC3@@users.noreply.github.com>
2025-04-21 12:58:08 +0200

    yes.

---
+++ SFLOW/skibidi.py
@@ -0,0 +1,45 @@
+import subprocess, socket
+from datetime import datetime, timezone
+from sqlmodel import SQLModel, Field, create_engine, Session
+from classess import data
+
+# DB conf
+DB_HOST = "192.168.1.63"
+DB_PORT = 3306
+DB_USER = "root"
+DB_PWD = "221411"
+DB_NAME = "Sflow_Data"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+
+# header (hex) parser
+def extract_ips_from_header_bytes_and_replace_with_something_more_humanReadable_plz_thanks(header_str):
+    try:
+        uber_unreadable_hex_thing = header_str.strip().split("-")
+        header_Howmany_bytes = bytes([int(x, 16) for x in uber_unreadable_hex_thing])
+
+        # If shorter than 34 bytes return empty
+        if len(header_Howmany_bytes) < 34:
+            return None, None, None, None, None
+
+        ip_header_start = 14
+        ip_header = header_Howmany_bytes[ip_header_start:]
+        ip_header_lennthggh = (ip_header[0] & 0x0F) * 4
+
+        src_ip = socket.inet_ntoa(ip_header[12:16])
+        dst_ip = socket.inet_ntoa(ip_header[16:20])
+        proto = ip_header[9]
+
+        transport_header_start = ip_header_start + ip_header_lennthggh
+        transport_header = header_Howmany_bytes[transport_header_start:]
+
+        src_port = int.from_bytes(transport_header[0:2], byteorder='big')
+        dst_port = int.from_bytes(transport_header[2:4], byteorder='big')
+
+        return src_ip, dst_ip, src_port, dst_port, proto
+
+
+    # nah
+    except Exception as e:
+        print(f"[!] Error decoding headerBytes: {e}")
+        return None, None, None, None, None
