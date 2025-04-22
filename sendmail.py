727bb98854545bb0735c028dda253cffec098824
ChirutoGH <201098990+ChirutoGH@@users.noreply.github.com>
2025-04-22 13:20:47 +0200

    sendmail.py kan nu skicka e-post till flera mottagare

--- sendmail.py
+++ sendmail.py
@@ -1,17 +1,17 @@
 import smtplib
 from email.mime.text import MIMEText
 
-def send_mail(subject, message, to_email):
-    from_mail = "dancing.data.mail@gmail.com"
+def send_mail(subject, message, recipient):
+    sender = "dancing.data.mail@gmail.com"
     from_password = ""
 
     msg = MIMEText(message, 'html')
     msg['Subject'] = subject
-    msg['From'] = from_mail
-    msg['To'] = to_email
+    msg['From'] = sender
+    msg['To'] = ",".join(recipient)
 
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
-        server.login(from_mail, from_password)
+        server.login(sender, from_password)
         server.send_message(msg)
         server.quit() 
