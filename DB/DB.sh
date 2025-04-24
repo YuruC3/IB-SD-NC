6a8e4858ba35b3f5bd393f0515ee9870f154e7fd
Bubbis03 <alexander@fyrpihl.se>
2025-04-24 08:51:31 +0200

    senaste

--- DB/DB.sh
+++ DB/DB.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Gammal, använd config-guiden istället.
 if [ $(/usr/bin/id -u) -ne 0 ]
  then
 	echo "Måste Köras med sudo"
@@ -13,10 +13,7 @@ service mysql restart
 mysql
 create database Netflow_Data;
 use Netflow_Data;
-create table data (ID int auto_increment, IPV4_SRC_ADDR varchar(100), IPV4_DST_ADDR varchar(100), NEXT_HOP varchar(100), INPUT varchar(100), OUTPUT varchar (100),
- IN_PACKETS varchar(100), IN_OCTETS varchar(100), FIRST_SWITCHED varchar(100), LAST_SWITCHED varchar(100), SRC_PORT varchar(100), DST_PORT varchar(100),
- TCP_FLAGS varchar(100), PROTO varchar(100), TOS varchar(100), SRC_AS varchar(100), DST_AS varchar(100), SRC_MASK varchar(100), DST_MASK varchar(100), DATE timestamp default current_timestamp, primary key(ID));
-
+create table data (ID int auto_increment, IPV4_SRC_ADDR varchar(100), IPV4_DST_ADDR varchar(100), NEXT_HOP varchar(100), INPUT varchar(100), OUTPUT varchar (100), IN_PACKETS varchar(100), IN_OCTETS varchar(100), FIRST_SWITCHED varchar(100), LAST_SWITCHED varchar(100), SRC_PORT varchar(100), DST_PORT varchar(100), TCP_FLAGS varchar(100), PROTO varchar(100), TOS varchar(100), SRC_AS varchar(100), DST_AS varchar(100), SRC_MASK varchar(100), DST_MASK varchar(100), DATE timestamp default current_timestamp, primary key(ID));
 create event prune_data on schedule every 1 day do delete from data where 2592000 <= UNIX_TIMESTAMP(UTC_DATE()) - UNIX_TIMESTAMP(DATE);
 create database Switch_Conf;
 use Switch_Conf;
